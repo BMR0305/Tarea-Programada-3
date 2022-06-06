b9eabf8e6a5b67e8e9e9f0286646a714332b0772
Ingrid Vargas Badilla <invabadilla@estudiantec.cr>
2022-06-05 19:31:28 -0600

    Creation of Server and bricks
    server creation, communication with java and assignment of powers to bricks

---
+++ server/Server/Server.c
@@ -0,0 +1,182 @@
+#undef UNICODE
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+// Need to link with Ws2_32.lib
+#pragma comment (lib, "Ws2_32.lib")
+// #pragma comment (lib, "Mswsock.lib")
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "8888"
+
+int __cdecl main(void) {
+
+    WSADATA wsaData;
+    int iResult;
+
+    SOCKET ListenSocket = INVALID_SOCKET;
+    SOCKET ClientSocket = INVALID_SOCKET;
+
+    struct addrinfo *result = NULL;
+    struct addrinfo hints;
+
+    int iSendResult;
+    char recvbuf[DEFAULT_BUFLEN];
+    int recvbuflen = DEFAULT_BUFLEN;
+
+    while(TRUE){
+
+        // Initialize Winsock
+        iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+        if (iResult != 0) {
+            printf("WSAStartup failed with error: %d\n", iResult);
+            return 1;
+        }
+
+        ZeroMemory(&hints, sizeof(hints));
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_protocol = IPPROTO_TCP;
+        hints.ai_flags = AI_PASSIVE;
+
+        // Resolve the server address and port
+        iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+        if (iResult != 0) {
+            printf("getaddrinfo failed with error: %d\n", iResult);
+            WSACleanup();
+            return 1;
+        }
+
+        // Create a SOCKET for connecting to server
+        ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+        if (ListenSocket == INVALID_SOCKET) {
+            printf("socket failed with error: %ld\n", WSAGetLastError());
+            freeaddrinfo(result);
+            WSACleanup();
+            return 1;
+        }
+
+        // Setup the TCP listening socket
+        iResult = bind(ListenSocket, result->ai_addr, (int) result->ai_addrlen);
+        if (iResult == SOCKET_ERROR) {
+            printf("bind failed with error: %d\n", WSAGetLastError());
+            freeaddrinfo(result);
+            closesocket(ListenSocket);
+            WSACleanup();
+            return 1;
+        }
+
+        freeaddrinfo(result);
+
+        iResult = listen(ListenSocket, SOMAXCONN);
+        if (iResult == SOCKET_ERROR) {
+            printf("listen failed with error: %d\n", WSAGetLastError());
+            closesocket(ListenSocket);
+            WSACleanup();
+            return 1;
+        }
+
+        // Accept a client socket
+        ClientSocket = accept(ListenSocket, NULL, NULL);
+        if (ClientSocket == INVALID_SOCKET) {
+            printf("accept failed with error: %d\n", WSAGetLastError());
+            closesocket(ListenSocket);
+            WSACleanup();
+            return 1;
+        }
+
+        // No longer need server socket
+        closesocket(ListenSocket);
+
+        // Receive until the peer shuts down the connection
+        do {
+
+            iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+            if (iResult > 0) {
+                printf("Bytes received: %d %s\n", iResult, recvbuf);
+
+                char *ptr;
+                float factor;
+                int level = strtol(recvbuf, &ptr, 10);
+                switch (level) {
+                    case 1:
+                        factor=1;
+                        break;
+                    case 2:
+                        factor=1.1;
+                        break;
+                    case 3:
+                        factor=1.2;
+                        break;
+                    case 4:
+                        factor=1.3;
+                        break;
+                    default:
+                        break;
+                }
+                char game[243];
+                memset(game, 0, 243);
+                char add = '%';
+                char num [3];
+                for (int i = 8; i > 0; --i) {
+                    int score =(int) (10*factor*i);
+                    for (int j = 0; j < 6; ++j) {
+                        int r = rand() % 7;
+                        add = r+'0';
+                        strncat(game, &add, 1);
+                        add = '-';
+                        strncat(game, &add, 1);
+                        sprintf(num, "%d", score);
+                        strncat(game, &num, 3);
+                        add = '%';
+                        strncat(game, &add, 1);
+                    }
+                }
+
+                printf("%s\n", game);
+
+
+                // Echo the buffer back to the sender
+                iSendResult = send(ClientSocket, game, 260, 0);
+
+                if (iSendResult == SOCKET_ERROR) {
+                    printf("send failed with error: %d\n", WSAGetLastError());
+                    closesocket(ClientSocket);
+                    WSACleanup();
+                    return 1;
+                }
+                printf("Bytes sent: %d\n", iSendResult);
+            } else if (iResult == 0)
+                printf("Connection closing...\n");
+            else {
+                printf("recv failed with error: %d\n", WSAGetLastError());
+                closesocket(ClientSocket);
+                WSACleanup();
+                return 1;
+            }
+
+        } while (iResult > 0);
+
+        // shutdown the connection since we're done
+        iResult = shutdown(ClientSocket, SD_SEND);
+        if (iResult == SOCKET_ERROR) {
+            printf("shutdown failed with error: %d\n", WSAGetLastError());
+            closesocket(ClientSocket);
+            WSACleanup();
+            return 1;
+        }
+
+    }
+    // cleanup
+    closesocket(ClientSocket);
+    WSACleanup();
+
+    return 0;
+}
